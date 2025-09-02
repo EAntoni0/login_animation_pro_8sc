22228768e3f7225f4719e7b17d9c4f09f9b3facf
EAntoni0 <ac817924@gmail.com>
2025-09-02 15:42:29 -0600

    feat(login): add email and password inputs

--- lib/screens/login-screen.dart
+++ lib/screens/login-screen.dart
@@ -10,14 +10,83 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(child: RiveAnimation.asset('assets/animated_login_character.riv')),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              //alineacion del eje vertical
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  //ancho de la pantalla calculado con wedia query
+                  width: size.width,
+                  height: 200,
+                  child: RiveAnimation.asset('assets/animated_login_character.riv')
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+            
+                    ),
+                  ),
+            
+                  SizedBox(height: 10),
+            
+                  TextField(
+                    //para ocultar la contraseña
+                    obscureText: _obscureText,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText ? Icons.visibility_off : Icons.visibility,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: size.width,
+                    child: const Text('Forgot Password?',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      decoration: TextDecoration.underline
+                    ),
+                    ),
+                  )
+
+              ],
+            ),
           )
       ),
     );
